88ef8610d5df53df20ffff0a5e9411614f7dc749
onionlee <rkdgml6332@gmail.com>
2016-01-27 23:41:08 +0900

    Import new package

---
+++ Sample/src/DialogueDescriptor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class DialogueTable
+{
+	[JsonProperty("Dialogue")]
+	public List<DialogueDescriptor> Dialogues { get; set; }
+}
+
+public class DialogueDescriptor
+{
+	[JsonProperty("ID")]
+	public string ID { get; set; }
+
+	[JsonProperty("Description")]
+	public string Description { get; set; }
+}
